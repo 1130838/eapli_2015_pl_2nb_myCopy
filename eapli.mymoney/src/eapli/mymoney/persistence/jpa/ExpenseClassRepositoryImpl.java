e0f05a295802eb023a7a5828a22c8a208289a428
1131265 <1131265@isep.ipp.pt>
2015-04-17 19:31:53 +0100

    UC-D-003: Visualização de gasto desta semana Implementação de repositório e interface de repositório.

--- eapli.mymoney/src/eapli/mymoney/persistence/jpa/ExpenseClassRepositoryImpl.java
+++ eapli.mymoney/src/eapli/mymoney/persistence/jpa/ExpenseClassRepositoryImpl.java
@@ -1,21 +1,22 @@
 package eapli.mymoney.persistence.jpa;
 
+import eapli.framework.model.Money;
 import eapli.mymoney.domain.Expense;
-import eapli.mymoney.domain.PaymentMethod;
+import eapli.mymoney.domain.Period;
 import eapli.mymoney.persistence.ExpenseRepository;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by brunodevesa on 16/04/15.
  */
 public class ExpenseClassRepositoryImpl implements ExpenseRepository {
 
+	@Override
+	public boolean registerExpense(Expense expense) {
+		return false;
+	}
 
-    @Override
-    public boolean registerExpense(Expense expense) {
-        return false;
-    }
+	@Override
+	public Money getWeekExpediture(Period period) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 }
