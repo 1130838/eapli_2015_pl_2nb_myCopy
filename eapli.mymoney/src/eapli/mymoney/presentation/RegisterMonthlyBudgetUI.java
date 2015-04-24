603cef03d3b6a5a96258749fe7b183aa382553f7
Jose Ferreira [1131097] <1131097@isep.ipp.pt>
2015-04-24 20:03:43 +0100

    ReisterMonthlyBidget simplificação do doShow

--- eapli.mymoney/src/eapli/mymoney/presentation/RegisterMonthlyBudgetUI.java
+++ eapli.mymoney/src/eapli/mymoney/presentation/RegisterMonthlyBudgetUI.java
@@ -18,53 +18,67 @@ import java.util.List;
  */
 public class RegisterMonthlyBudgetUI extends BaseUI {
 
-    private RegisterMonthlyBudgetController controller = new RegisterMonthlyBudgetController();
-    private List<ExpenseType> expenseTypeList;
-    private String budgetName;
+	private RegisterMonthlyBudgetController controller = new RegisterMonthlyBudgetController();
+	private List<ExpenseType> expenseTypeList;
+	private String budgetName;
+	ExpenseType chosenExpenseType;
 
-    @Override
-    public boolean doShow() {
-        System.out.println("\n" + headline());
+	@Override
 
-        budgetName = Console.readLine("Type the Budget name:");
-        controller.changeBudgetName(budgetName);
-        expenseTypeList = controller.getExpenseTypes();
-        int option = -1, position = 1;
+	public boolean doShow() {
+		System.out.println("\n" + headline());
+		budgetName = Console.readLine("Type the Budget name:");
+		controller.changeBudgetName(budgetName);
+		expenseTypeList = controller.getExpenseTypes();
+		int option = -1, position = 1;
+		chosenExpenseType = new ExpenseType("");
+		while (option != 0) {
+			boolean bExpenseTypeChosen = false;
+			if (!chooseExpenseType()) {
+				break;
+			}
+			double chosenValue = Console.
+				readDouble("Please insert a Value for this Expense:");
+			Money value = Money.euros(chosenValue);
+			//
+			controller.addEntry(chosenExpenseType, value);
+		}
+		submit();
+		return true;
+	}
 
-        while (option != 0) {
-            position = 1;
-            for (Iterator<ExpenseType> iterator = expenseTypeList.iterator(); iterator.hasNext();) {
-                ExpenseType next = iterator.next();
-                System.out.println(position + " - " + next.description());
-                position++;
-            }
-            option = Console.readInteger("Please choose an Expense Type (0 to sop inserting...)");
-            if (option == 0) {
-                break;
-            }
-            ExpenseType chosenExpenseType = null;
-            for (Iterator<ExpenseType> iterator = expenseTypeList.iterator(); iterator.hasNext();) {
-                position++;
-                if (option == position) {
-                    chosenExpenseType = iterator.next();
-                }
-            }
-            double chosenValue = Console.readDouble("Please insert a Value for this Expense:");
-            Money value = Money.euros(chosenValue);
-            //
-            controller.addEntry(chosenExpenseType, value);
-        }
-        submit();
-        return true;
-    }
+	private void submit() {
+		controller.saveBudget();
+		System.out.println("\nMonthly Budget recorded!");
+	}
 
-    private void submit() {
-        controller.saveBudget();
-        System.out.println("\nMonthly Budget recorded!");
-    }
+	@Override
+	public String headline() {
+		return "REGISTER MONTLHY BUDGET";
+	}
 
-    @Override
-    public String headline() {
-        return "REGISTER MONTLHY BUDGET";
-    }
+	public boolean chooseExpenseType() {
+		int position = 1;
+		for (Iterator<ExpenseType> iterator = expenseTypeList.iterator(); iterator.
+			hasNext();) {
+			ExpenseType next = iterator.next();
+			System.out.println(position + " - " + next.description());
+			position++;
+		}
+		int option = Console.
+			readInteger("Please choose an Expense Type (0 to stop inserting...)");
+		if (option == 0) {
+			return false;
+		}
+
+		for (Iterator<ExpenseType> iterator = expenseTypeList.iterator(); iterator.
+			hasNext();) {
+			position++;
+			if (option == position) {
+				chosenExpenseType = iterator.next();
+			}
+		}
+		return true;
+	}
+
 }
