c76e53e6350e6d6ff31beec02e486d81038bc3b5
Nuno Nogueira <1111785@isep.ipp.pt>
2015-05-14 22:09:18 +0100

    metodo para fazer disable

--- eapli.mymoney/src/eapli/mymoney/presentation/RegisterExpenseGroupUI.java
+++ eapli.mymoney/src/eapli/mymoney/presentation/RegisterExpenseGroupUI.java
@@ -16,54 +16,57 @@ import java.util.Calendar;
  */
 public class RegisterExpenseGroupUI extends BaseUI {
 
-	/**
-	 * Controller of use case.
-	 */
-	private final RegisterExpenseGroupController controller
-		= new RegisterExpenseGroupController();
-	/**
-	 * The name of the Expense Group.
-	 */
-	private String expenseGroup;
-	/**
-	 * Begin of period.
-	 */
-	private Calendar beginPeriod;
-	/**
-	 * End of period.
-	 */
-	private Calendar endPeriod;
-	/**
-	 * The value of the budget estimation.
-	 */
-	private BigDecimal estimation;
+    /**
+     * Controller of use case.
+     */
+    private final RegisterExpenseGroupController controller
+            = new RegisterExpenseGroupController();
+    /**
+     * The name of the Expense Group.
+     */
+    private String expenseGroup;
+    /**
+     * Begin of period.
+     */
+    private Calendar beginPeriod;
+    /**
+     * End of period.
+     */
+    private Calendar endPeriod;
+    /**
+     * The value of the budget estimation.
+     */
+    private BigDecimal estimation;
 
-	@Override
-	public final boolean doShow() {
-		expenseGroup = Console.
-			readLine("Enter expense group description » ");
-		beginPeriod = Console.
-			readCalendar("Enter expense group begin period (dd-MM-yyyy) » ");
-		endPeriod = Console.
-			readCalendar("Enter expense group end period (dd-MM-yyyy) » ");
-		estimation = new BigDecimal(Console.readDouble(
-			"Enter expense group budget estimation » "));
-		submit();
+    //1 to enable by default , o to disable
+    private int disable = 1;
 
-		return true;
-	}
+    @Override
+    public final boolean doShow() {
+        expenseGroup = Console.
+                readLine("Enter expense group description » ");
+        beginPeriod = Console.
+                readCalendar("Enter expense group begin period (dd-MM-yyyy) » ");
+        endPeriod = Console.
+                readCalendar("Enter expense group end period (dd-MM-yyyy) » ");
+        estimation = new BigDecimal(Console.readDouble(
+                "Enter expense group budget estimation » "));
+        submit();
 
-	/**
-	 * Records the Group.
-	 */
-	private void submit() {
-		controller.registerExpenseGroup(expenseGroup, beginPeriod,
-										endPeriod, estimation);
-		System.out.println("\nExpense group recorded!");
-	}
+        return true;
+    }
 
-	@Override
-	public final String headline() {
-		return "REGISTER AN EXPENSE GROUP";
-	}
+    /**
+     * Records the Group.
+     */
+    private void submit() {
+        controller.registerExpenseGroup(expenseGroup, beginPeriod,
+                endPeriod, estimation);
+        System.out.println("\nExpense group recorded!");
+    }
+
+    @Override
+    public final String headline() {
+        return "REGISTER AN EXPENSE GROUP";
+    }
 }
