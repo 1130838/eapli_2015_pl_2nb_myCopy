8fda0da63fdc476f139010e4b153213b52be2491
1131265 <1131265@isep.ipp.pt>
2015-05-15 19:02:28 +0100

    Missing test opt.

--- eapli.mymoney/test/eapli/mymoney/persistence/ExpenseGroupRepositoryTest.java
+++ eapli.mymoney/test/eapli/mymoney/persistence/ExpenseGroupRepositoryTest.java
@@ -10,10 +10,11 @@ import java.util.Iterator;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -21,103 +22,109 @@ import static org.junit.Assert.*;
  */
 public class ExpenseGroupRepositoryTest {
 
-    public ExpenseGroupRepositoryTest() {
-    }
+	public ExpenseGroupRepositoryTest() {
+	}
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
+	@BeforeClass
+	public static void setUpClass() {
+	}
 
-    @AfterClass
-    public static void tearDownClass() {
-    }
+	@AfterClass
+	public static void tearDownClass() {
+	}
 
-    private transient ExpenseGroupRepository instance;
+	private transient ExpenseGroupRepository instance;
 
-    @Before
-    public void setUp() {
-        instance = Persistence.getRepositoryFactory().getExpenseGroupRepository();
-    }
+	@Before
+	public void setUp() {
+		instance = Persistence.getRepositoryFactory().
+			getExpenseGroupRepository();
+	}
 
-    @After
-    public void tearDown() {
-    }
+	@After
+	public void tearDown() {
+	}
 
-    /**
-     * Test of add method, of class ExpenseGroupRepository.
-     */
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        ExpenseGroup expenseGroup = null;
-        ExpenseGroupRepository instance = new ExpenseGroupRepositoryImpl();
-        boolean expResult = false;
-        boolean result = instance.add(expenseGroup);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	/**
+	 * Test of add method, of class ExpenseGroupRepository.
+	 */
+	@Test
+	public void testAdd() {
+		System.out.println("add");
+		ExpenseGroup expenseGroup = null;
+		ExpenseGroupRepository instance = new ExpenseGroupRepositoryImpl();
+		boolean expResult = false;
+		boolean result = instance.add(expenseGroup);
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
 
-    /**
-     * Test of size method, of class ExpenseGroupRepository.
-     */
-    @Test
-    public void testSize() {
-        System.out.println("size");
-        ExpenseGroupRepository instance = new ExpenseGroupRepositoryImpl();
-        long expResult = 0L;
-        long result = instance.size();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	/**
+	 * Test of size method, of class ExpenseGroupRepository.
+	 */
+	@Test
+	public void testSize() {
+		System.out.println("size");
+		ExpenseGroupRepository instance = new ExpenseGroupRepositoryImpl();
+		long expResult = 0L;
+		long result = instance.size();
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
 
-    /**
-     * Test of all method, of class ExpenseGroupRepository.
-     */
-    @Test
-    public void testAll() {
-        System.out.println("all");
-        ExpenseGroupRepository instance = new ExpenseGroupRepositoryImpl();
-        List<ExpenseGroup> expResult = null;
-        List<ExpenseGroup> result = instance.all();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	/**
+	 * Test of all method, of class ExpenseGroupRepository.
+	 */
+	@Test
+	public void testAll() {
+		System.out.println("all");
+		ExpenseGroupRepository instance = new ExpenseGroupRepositoryImpl();
+		List<ExpenseGroup> expResult = null;
+		List<ExpenseGroup> result = instance.all();
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
 
-    /**
-     * Test of iterator method, of class ExpenseGroupRepository.
-     */
-    @Test
-    public void testIterator() {
-        System.out.println("iterator");
-        int pagesize = 0;
-        ExpenseGroupRepository instance = new ExpenseGroupRepositoryImpl();
-        Iterator<ExpenseGroup> expResult = null;
-        Iterator<ExpenseGroup> result = instance.iterator(pagesize);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+	/**
+	 * Test of iterator method, of class ExpenseGroupRepository.
+	 */
+	@Test
+	public void testIterator() {
+		System.out.println("iterator");
+		int pagesize = 0;
+		ExpenseGroupRepository instance = new ExpenseGroupRepositoryImpl();
+		Iterator<ExpenseGroup> expResult = null;
+		Iterator<ExpenseGroup> result = instance.iterator(pagesize);
+		assertEquals(expResult, result);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
 
-    public class ExpenseGroupRepositoryImpl implements ExpenseGroupRepository {
+	public class ExpenseGroupRepositoryImpl implements ExpenseGroupRepository {
 
-        public boolean add(ExpenseGroup expenseGroup) {
-            return false;
-        }
+		public boolean add(ExpenseGroup expenseGroup) {
+			return false;
+		}
 
-        public long size() {
-            return 0L;
-        }
+		public long size() {
+			return 0L;
+		}
 
-        public List<ExpenseGroup> all() {
-            return null;
-        }
+		public List<ExpenseGroup> all() {
+			return null;
+		}
 
-        public Iterator<ExpenseGroup> iterator(int pagesize) {
-            return null;
-        }
-    }
+		public Iterator<ExpenseGroup> iterator(int pagesize) {
+			return null;
+		}
+
+		@Override
+		public ExpenseGroup findById(String id) {
+			throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		}
+	}
 
 }
